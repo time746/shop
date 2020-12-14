57d71f2ef4cd20d1280378951961992f14f588d2
time746 <746774077@qq.com>
2020-12-14 20:57:11 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+// ele组件
+import { Button, Select } from 'element-ui';
+Vue.component(Button.name, Button);
+Vue.component(Select.name, Select);
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+
+Vue.use(VueRouter)
+
+
+const routes = [
+
+]
+
+const router = {
+  routes,
+  mode: history
+}
+
+export default router
